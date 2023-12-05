6f19d41bce66faa4e79c14053e56cf5a530e085a
JasperDeSutter <jasper.desutter@gmail.com>
2023-12-05 13:48:20 +0100

    day05: part 2

--- src/day05.zig
+++ src/day05.zig
@@ -5,11 +5,13 @@ pub const main = runner.run(solve);
 
 fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror!void {
     const result = try lowestLocation(alloc, input);
-    std.debug.print("05 lowest location: {}\n", .{result});
+    std.debug.print("05 lowest location: {}\n", .{result[0]});
+    std.debug.print("05 lowest location range: {}\n", .{result[1]});
 }
 
-fn lowestLocation(alloc: std.mem.Allocator, input: []const u8) !usize {
+fn lowestLocation(alloc: std.mem.Allocator, input: []const u8) ![2]usize {
     var result: usize = std.math.maxInt(usize);
+    var resultRange: usize = std.math.maxInt(usize);
 
     var lines = std.mem.split(u8, input, "\n");
     const seedsLine = lines.next().?;
@@ -21,39 +23,81 @@ fn lowestLocation(alloc: std.mem.Allocator, input: []const u8) !usize {
         }
         mapping.deinit();
     }
-    var last: *std.ArrayList([3]usize) = undefined;
+    var last: ?*std.ArrayList([3]usize) = null;
 
     while (lines.next()) |line| {
         if (line.len == 0) {
-            last = try mapping.addOne();
-            last.* = std.ArrayList([3]usize).init(alloc);
+            const ptr = try mapping.addOne();
+            ptr.* = std.ArrayList([3]usize).init(alloc);
+            last = ptr;
         } else if (std.ascii.isDigit(line[0])) {
             var nums = std.mem.split(u8, line, " ");
             const first = try std.fmt.parseInt(usize, nums.next().?, 10);
             const second = try std.fmt.parseInt(usize, nums.next().?, 10);
-            const third = try std.fmt.parseInt(usize, nums.next().?, 10);
-            try last.append(.{ first, second, second + third });
+            const third = second + try std.fmt.parseInt(usize, nums.next().?, 10);
+
+            var l = last.?;
+            for (l.items, 0..) |item, i| {
+                if (item[1] > second) {
+                    try l.insert(i, .{ first, second, third });
+                    break;
+                }
+            } else {
+                try l.append(.{ first, second, third });
+            }
         }
     }
 
     var seeds = std.mem.split(u8, seedsLine[7..], " ");
-    while (seeds.next()) |seed| {
-        var value = try std.fmt.parseInt(usize, seed, 10);
+    var prev: ?usize = null;
+    while (seeds.next()) |seed_str| {
+        const seed = try std.fmt.parseInt(usize, seed_str, 10);
+        const single = mapValue(seed, mapping.items);
+        if (single < result) result = single;
 
-        for (mapping.items) |item| {
-            for (item.items) |map| {
-                if (map[1] <= value and map[2] > value) {
+        if (prev) |start| {
+            for (start..start + seed) |i| {
+                const value = mapValue(i, mapping.items);
+                if (value < resultRange) {
+                    resultRange = value;
+                }
+            }
+
+            prev = null;
+        } else {
+            prev = seed;
+        }
+    }
+
+    return .{ result, resultRange };
+}
+
+fn mapValue(startValue: usize, mapping: []const std.ArrayList([3]usize)) usize {
+    var value = startValue;
+    for (mapping) |item| {
+        if (item.items[0][1] < value and item.items[item.items.len - 1][2] < value) {
+            continue;
+        }
+        var left: usize = 0;
+        var right: usize = item.items.len;
+
+        while (left < right) {
+            const mid = left + (right - left) / 2;
+
+            const map = item.items[mid];
+            if (map[1] <= value) {
+                if (map[2] > value) {
                     value += map[0];
                     value -= map[1];
                     break;
                 }
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-
-        if (value < result) result = value;
     }
-
-    return result;
+    return value;
 }
 
 test {
@@ -95,5 +139,7 @@ test {
 
     const example_result: usize = 35;
     const result = try lowestLocation(std.testing.allocator, input);
-    try std.testing.expectEqual(example_result, result);
+    try std.testing.expectEqual(example_result, result[0]);
+    const example_result_range: usize = 46;
+    try std.testing.expectEqual(example_result_range, result[1]);
 }
