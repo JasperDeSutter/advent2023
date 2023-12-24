b802537a9edec900ef63cef88fc955dc50ecf8fd
JasperDeSutter <jasper.desutter@gmail.com>
2023-12-24 14:38:54 +0100

    day21: part2

--- src/day21.zig
+++ src/day21.zig
@@ -4,50 +4,75 @@ const runner = @import("runner.zig");
 pub const main = runner.run("21", solve);
 
 fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
-    return impl(alloc, input, 64);
+    var samples = try impl(alloc, input, &.{ 64, 65, 252, 253 });
+    defer samples.deinit(alloc);
+
+    const even_corners = samples.items[2] - samples.items[0];
+    const odd_corners = samples.items[3] - samples.items[1];
+
+    const even_full = samples.items[2];
+    const odd_full = samples.items[3];
+
+    const lineLen = std.mem.indexOfScalar(u8, input, '\n').?;
+    const n = ((26501365 - (lineLen / 2)) / lineLen);
+
+    const p2 = ((n + 1) * (n + 1)) * odd_full + (n * n) * even_full - (n + 1) * odd_corners + n * even_corners;
+
+    return .{ samples.items[0], p2 };
 }
 
-fn impl(alloc: std.mem.Allocator, input: []const u8, steps: u8) anyerror![2]usize {
+fn impl(alloc: std.mem.Allocator, input: []const u8, samplePoints: []const u8) anyerror!std.ArrayListUnmanaged(usize) {
+    const lineLen: u16 = @intCast(std.mem.indexOfScalar(u8, input, '\n').? + 1);
+    const startPos: u16 = @intCast(std.mem.indexOfScalar(u8, input, 'S').?);
+
     const map = try alloc.alloc(u8, input.len);
     defer alloc.free(map);
     @memset(map, 0);
 
-    const lineLen = std.mem.indexOfScalar(u8, input, '\n').? + 1;
-    const startPos = std.mem.indexOfScalar(u8, input, 'S').?;
-    map[startPos] = 1;
+    const Tuple = struct { i: u16, step: u8 };
+    var stack = try std.ArrayListUnmanaged(Tuple).initCapacity(alloc, 4);
+    defer stack.deinit(alloc);
+    stack.appendAssumeCapacity(.{ .i = startPos, .step = 1 });
 
-    for (2..(steps + 2)) |stepU| {
-        const step: u8 = @intCast(stepU);
-        for (0..input.len) |i| {
-            if (input[i] == '#' or input[i] == '\n') continue;
-            if (map[i] != 0) continue;
+    var stackIndex: usize = 0;
+    while (stack.items.len > stackIndex) {
+        const item = stack.items[stackIndex];
+        stackIndex += 1;
+        if (map.len <= item.i or map[item.i] != 0 or input[item.i] == '#' or input[item.i] == '\n') continue;
 
-            const prev = step - 1;
+        map[item.i] = item.step;
 
-            if (i > 0 and map[i - 1] == prev) {
-                map[i] = step;
-            }
-            if (i < map.len - 1 and map[i + 1] == prev) {
-                map[i] = step;
-            }
-            if (i >= lineLen and map[i - lineLen] == prev) {
-                map[i] = step;
-            }
-            if (i + lineLen < map.len and map[i + lineLen] == prev) {
-                map[i] = step;
-            }
+        if (stack.capacity < stack.items.len + 4) {
+            // discard old items while resizing
+            var old = stack;
+            stack = try std.ArrayListUnmanaged(Tuple).initCapacity(alloc, old.capacity * 2);
+            stack.items.len = old.items.len - stackIndex;
+            @memcpy(stack.items[0 .. old.items.len - stackIndex], old.items[stackIndex..]);
+            old.deinit(alloc);
+            stackIndex = 0;
         }
+
+        const nextStep = item.step + 1;
+        stack.appendSliceAssumeCapacity(&.{
+            .{ .i = item.i -% 1, .step = nextStep },
+            // thx zigfmt
+            .{ .i = item.i +% 1, .step = nextStep },
+            .{ .i = item.i -% lineLen, .step = nextStep },
+            .{ .i = item.i +% lineLen, .step = nextStep },
+        });
     }
 
-    var result: usize = 0;
-    const odd = steps % 2 == 0;
-    for (map) |v| {
-        if (v != 0 and ((odd and v % 2 == 1) or (!odd and v % 2 == 0))) {
-            result += 1;
+    var samples = try std.ArrayListUnmanaged(usize).initCapacity(alloc, samplePoints.len);
+    for (samplePoints) |steps| {
+        var result: usize = 0;
+        const odd = 1 - (steps % 2);
+        for (map) |v| {
+            if (v > 0 and v % 2 == odd and v < (steps + 2)) result += 1;
         }
+        samples.appendAssumeCapacity(result);
     }
 
-    return .{ result, 0 };
+    return samples;
 }
 
 test {
@@ -65,7 +90,8 @@ test {
         \\...........
     ;
 
-    const result = try impl(std.testing.allocator, input, 6);
+    var result = try impl(std.testing.allocator, input, &.{6});
+    defer result.deinit(std.testing.allocator);
     const example_result: usize = 16;
-    try std.testing.expectEqual(example_result, result[0]);
+    try std.testing.expectEqual(example_result, result.items[0]);
 }
