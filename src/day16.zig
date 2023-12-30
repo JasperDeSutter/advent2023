c8b5e9809cf6071ba34764725a781352d687622d
JasperDeSutter <jasper.desutter@gmail.com>
2023-12-30 16:05:02 +0100

    day16: part 2

--- src/day16.zig
+++ src/day16.zig
@@ -4,25 +4,48 @@ const runner = @import("runner.zig");
 pub const main = runner.run("16", solve);
 
 fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
-    const visited = try alloc.alloc(u8, input.len);
+    const visited = try alloc.alloc(bool, input.len);
     defer alloc.free(visited);
-    @memset(visited, 0);
 
     const stride: usize = @intCast(std.mem.indexOfScalar(u8, input, '\n').? + 1);
 
-    const Beam = struct {
-        dir: u8,
-        i: usize,
-    };
-    var beams = std.ArrayListUnmanaged(Beam){};
-    defer beams.deinit(alloc);
+    var beams = std.ArrayList(Beam).init(alloc);
+    defer beams.deinit();
 
-    const right: u8 = 0;
-    const down: u8 = 1;
-    const left: u8 = 2;
-    const up: u8 = 3;
+    var highest: usize = 0;
 
-    try beams.append(alloc, .{ .dir = right, .i = std.math.maxInt(usize) });
+    const rows = (input.len + 1) / stride;
+    for (0..rows) |row| {
+        const off = row * stride;
+        const a = try runBeams(visited, &beams, stride, input, .{ .dir = right, .i = off +% std.math.maxInt(usize) });
+        const b = try runBeams(visited, &beams, stride, input, .{ .dir = left, .i = off + stride - 1 });
+        highest = @max(highest, @max(a, b));
+    }
+
+    for (0..(stride - 1)) |col| {
+        const a = try runBeams(visited, &beams, stride, input, .{ .dir = down, .i = col -% stride });
+        const b = try runBeams(visited, &beams, stride, input, .{ .dir = up, .i = col + input.len + 1 });
+        highest = @max(highest, @max(a, b));
+    }
+
+    return .{ try runBeams(visited, &beams, stride, input, .{ .dir = right, .i = std.math.maxInt(usize) }), highest };
+}
+
+const right: u8 = 0;
+const down: u8 = 1;
+const left: u8 = 2;
+const up: u8 = 3;
+
+const Beam = struct {
+    dir: u8,
+    i: usize,
+};
+
+fn runBeams(visited: []bool, beams: *std.ArrayList(Beam), stride: usize, input: []const u8, init: Beam) !usize {
+    // std.debug.print("init: {}, {}\n", .{ init.dir, init.i });
+    try beams.append(init);
+    @memset(visited, false);
+
     while (beams.popOrNull()) |beam| {
         const inc: usize = switch (beam.dir) {
             right => 1,
@@ -37,10 +60,8 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
             const c = input[i];
             if (c == '\n') break;
 
-            const one: u8 = 1;
-            const flag = one << @intCast(beam.dir);
-            if (visited[i] & flag > 0) break;
-            visited[i] |= flag;
+            if ((c == '-' or c == '|') and visited[i]) break;
+            visited[i] = true;
 
             if (c == '/') {
                 const newDir = switch (beam.dir) {
@@ -50,7 +71,7 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
                     up => right,
                     else => unreachable,
                 };
-                try beams.append(alloc, .{ .dir = newDir, .i = i });
+                try beams.append(.{ .dir = newDir, .i = i });
                 break;
             }
             if (c == '\\') {
@@ -61,24 +82,23 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
                     up => left,
                     else => unreachable,
                 };
-                try beams.append(alloc, .{ .dir = newDir, .i = i });
+                try beams.append(.{ .dir = newDir, .i = i });
                 break;
             }
             if (c == '-' and (beam.dir == up or beam.dir == down)) {
-                try beams.appendSlice(alloc, &.{ .{ .dir = left, .i = i }, .{ .dir = right, .i = i } });
+                try beams.appendSlice(&.{ .{ .dir = left, .i = i }, .{ .dir = right, .i = i } });
                 break;
             }
             if (c == '|' and (beam.dir == left or beam.dir == right)) {
-                try beams.appendSlice(alloc, &.{ .{ .dir = up, .i = i }, .{ .dir = down, .i = i } });
+                try beams.appendSlice(&.{ .{ .dir = up, .i = i }, .{ .dir = down, .i = i } });
                 break;
             }
         }
     }
 
     var total_visited: usize = 0;
-    for (visited) |v| total_visited += @intFromBool(v != 0);
-
-    return .{ total_visited, 0 };
+    for (visited) |v| total_visited += @intFromBool(v);
+    return total_visited;
 }
 
 test {
@@ -98,4 +118,6 @@ test {
     const result = try solve(std.testing.allocator, input);
     const example_result: usize = 46;
     try std.testing.expectEqual(example_result, result[0]);
+    const example_result2: usize = 51;
+    try std.testing.expectEqual(example_result2, result[1]);
 }
