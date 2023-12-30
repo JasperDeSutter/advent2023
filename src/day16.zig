df6baf69541194858e95b2ed8943a9b6ec71abc5
JasperDeSutter <jasper.desutter@gmail.com>
2023-12-30 17:40:50 +0100

    day16: perf

--- src/day16.zig
+++ src/day16.zig
@@ -12,23 +12,36 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
     var beams = std.ArrayList(Beam).init(alloc);
     defer beams.deinit();
 
+    var skips = std.ArrayListUnmanaged(usize){};
+    defer skips.deinit(alloc);
+
+    const firstBeam = try runBeams(visited, &beams, stride, input, .{ .dir = right, .i = std.math.maxInt(usize) }, &skips);
+    try skips.append(alloc, std.math.maxInt(usize));
+
     var highest: usize = 0;
+
+    // beams are checked from top to bottom so skips only need
+    for (0..(stride - 1)) |col| {
+        const a = try runBeams(visited, &beams, stride, input, .{ .dir = down, .i = col -% stride }, &skips);
+        highest = @max(highest, a);
+    }
 
     const rows = (input.len + 1) / stride;
     for (0..rows) |row| {
         const off = row * stride;
-        const a = try runBeams(visited, &beams, stride, input, .{ .dir = right, .i = off +% std.math.maxInt(usize) });
-        const b = try runBeams(visited, &beams, stride, input, .{ .dir = left, .i = off + stride - 1 });
+        const a = try runBeams(visited, &beams, stride, input, .{ .dir = right, .i = off +% std.math.maxInt(usize) }, &skips);
+        const b = try runBeams(visited, &beams, stride, input, .{ .dir = left, .i = off + stride - 1 }, &skips);
         highest = @max(highest, @max(a, b));
     }
 
     for (0..(stride - 1)) |col| {
-        const a = try runBeams(visited, &beams, stride, input, .{ .dir = down, .i = col -% stride });
-        const b = try runBeams(visited, &beams, stride, input, .{ .dir = up, .i = col + input.len + 1 });
-        highest = @max(highest, @max(a, b));
+        const b = try runBeams(visited, &beams, stride, input, .{ .dir = up, .i = col + input.len + 1 }, &skips);
+        highest = @max(highest, b);
     }
 
-    return .{ try runBeams(visited, &beams, stride, input, .{ .dir = right, .i = std.math.maxInt(usize) }), highest };
+    std.debug.print("skips: {any}\n", .{skips.items.len});
+
+    return .{ firstBeam, highest };
 }
 
 const right: u8 = 0;
@@ -41,64 +54,106 @@ const Beam = struct {
     i: usize,
 };
 
-fn runBeams(visited: []bool, beams: *std.ArrayList(Beam), stride: usize, input: []const u8, init: Beam) !usize {
-    // std.debug.print("init: {}, {}\n", .{ init.dir, init.i });
+fn runBeams(visited: []bool, beams: *std.ArrayList(Beam), stride: usize, input: []const u8, init: Beam, skips: *std.ArrayListUnmanaged(usize)) !usize {
+    const skip = find(skips, init.i);
+    if (skip < skips.items.len and skips.items[skip] == init.i) return 0;
     try beams.append(init);
     @memset(visited, false);
 
+    var total_visited: usize = 0;
+
     while (beams.popOrNull()) |beam| {
-        const inc: usize = switch (beam.dir) {
-            right => 1,
-            down => stride,
-            left => std.math.maxInt(usize),
-            up => std.math.maxInt(usize) - stride + 1,
-            else => unreachable,
-        };
         var i: usize = beam.i;
-        i +%= inc;
-        while (i < input.len) : (i +%= inc) {
-            const c = input[i];
-            if (c == '\n') break;
+        var dir = beam.dir;
+        outer: while (true) {
+            const inc: usize = switch (dir) {
+                right => 1,
+                down => stride,
+                left => std.math.maxInt(usize),
+                up => std.math.maxInt(usize) - stride + 1,
+                else => unreachable,
+            };
+            i +%= inc;
+            while (i < input.len) : (i +%= inc) {
+                const c = input[i];
+                if (c == '\n') {
+                    try addSkip(skips, i, beams.allocator);
+                    break :outer;
+                }
 
-            if ((c == '-' or c == '|') and visited[i]) break;
-            visited[i] = true;
+                const v = visited[i];
+                if ((c == '-' or c == '|') and v) break :outer;
+                if (!v) {
+                    total_visited += 1;
+                    visited[i] = true;
+                }
 
-            if (c == '/') {
-                const newDir = switch (beam.dir) {
-                    right => up,
-                    down => left,
-                    left => down,
-                    up => right,
-                    else => unreachable,
-                };
-                try beams.append(.{ .dir = newDir, .i = i });
-                break;
-            }
-            if (c == '\\') {
-                const newDir = switch (beam.dir) {
-                    right => down,
-                    down => right,
-                    left => up,
-                    up => left,
-                    else => unreachable,
-                };
-                try beams.append(.{ .dir = newDir, .i = i });
-                break;
-            }
-            if (c == '-' and (beam.dir == up or beam.dir == down)) {
-                try beams.appendSlice(&.{ .{ .dir = left, .i = i }, .{ .dir = right, .i = i } });
-                break;
-            }
-            if (c == '|' and (beam.dir == left or beam.dir == right)) {
-                try beams.appendSlice(&.{ .{ .dir = up, .i = i }, .{ .dir = down, .i = i } });
+                if (c == '/') {
+                    dir = switch (dir) {
+                        right => up,
+                        down => left,
+                        left => down,
+                        up => right,
+                        else => unreachable,
+                    };
+                    break;
+                }
+                if (c == '\\') {
+                    dir = switch (dir) {
+                        right => down,
+                        down => right,
+                        left => up,
+                        up => left,
+                        else => unreachable,
+                    };
+                    break;
+                }
+                if (c == '-' and (dir == up or dir == down)) {
+                    try beams.append(.{ .dir = left, .i = i });
+                    dir = right;
+                    break;
+                }
+                if (c == '|' and (dir == left or dir == right)) {
+                    try beams.append(.{ .dir = up, .i = i });
+                    dir = down;
+                    break;
+                }
+            } else {
+                try addSkip(skips, i, beams.allocator);
                 break;
             }
         }
     }
 
-    var total_visited: usize = 0;
-    for (visited) |v| total_visited += @intFromBool(v);
     return total_visited;
+}
+
+fn find(skips: *std.ArrayListUnmanaged(usize), i: usize) usize {
+    var l: usize = 0;
+    var r = skips.items.len;
+    while (l < r) {
+        const mid = (l + r) / 2;
+        const mid_val = skips.items[mid];
+        if (mid_val < i) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+
+    return l;
+    // var j: usize = 0;
+    // while (j < skips.items.len) : (j += 1) {
+    //     if (skips.items[j] == i) return j;
+    // }
+    // return j + 1;
+}
+
+fn addSkip(skips: *std.ArrayListUnmanaged(usize), i: usize, alloc: std.mem.Allocator) !void {
+    const s = find(skips, i);
+    if (s < skips.items.len and skips.items[s] == i) return;
+    try skips.insert(alloc, s, i);
+    // try skips.append(alloc, i);
 }
 
 test {
